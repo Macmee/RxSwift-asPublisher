05090cb86e01877f22c4867f427fd30090b9d044
David Zorychta <macmee@gmail.com>
2022-01-04 22:53:01 -0400

    Initial Commit

---
+++ Shared/TestRxWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRxWithSwiftUIApp.swift
+//  Shared
+//
+//  Created by David Zorychta on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TestRxWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
